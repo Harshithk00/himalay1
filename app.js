ed0334939d4c230a5f0d8b96f1d1c7ec3aa0ef58
Harshit <61313363+Harshithk00@users.noreply.github.com>
2024-07-24 19:05:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hellfffo World!')
+})
+
+app.listen(process.env.PORT, () => {
+    
+})
+app.get('/himalay', (req, res) => {
+    res.send('nothuing')
+})
